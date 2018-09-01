b3a990c6c82a826b6de8348ec24628ede1087e64
Truc4 <curtreyes00@yahoo.com>
2018-08-31 20:03:14 -0600

    Add files via upload

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -14,39 +14,38 @@ var angle_vector = Vector2()
 var one_shot = false
 
 #Project Resolution
-var res = Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
+var res
 
 func _physics_process(delta):
+	#Get Resolution
+	res = Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
+
 	#Debug
 	
+
 	#Movement
-	
+
 	#Move right
 	if Input.is_action_pressed("ui_right"):
 		vel.x += SPEED
 	#Move left
 	if Input.is_action_pressed("ui_left"):
 		vel.x += -SPEED
-	
+
 	#Gravity
-	if !is_on_floor():
-		vel.y += GRAVITY
-		one_shot = false
-		print("In air")
-	elif !one_shot:
-		vel.y = 0
-		one_shot = true
-		print("Stopped Y velocity")
-	if is_on_floor():
-		#Stop sideways velocity
-		vel.x *= .8
-	
+	vel.y += GRAVITY
+
 	#Move
 	move_and_slide(vel, Vector2(0, -1))
-	
-	#Get mouse position relative to player
+
+	if is_on_floor():
+		#Stop sideways velocity
+		vel.y = 0
+		vel.x *= .8
+
+	#Get mouse position relative to center of screen
 	mouse_pos = Vector2(get_viewport().get_mouse_position().x - (res.x/2), get_viewport().get_mouse_position().y - (res.y/2))
-	
+
 	#RotateGun
 	angle = atan2(mouse_pos.x, -mouse_pos.y)*(180/PI)
 	angle_vector = Vector2(sin(angle*(PI/180)), cos(angle*(PI/180)))
@@ -57,4 +56,5 @@ func _input(event):
 	"""if event.get_class() != 'InputEventMouseMotion':
 		print(event.get_class())"""
 	if event.is_action_pressed("ui_shoot"):
-		vel += Vector2(-angle_vector.x * KICKBACK, angle_vector.y * KICKBACK)
+		vel.x += -angle_vector.x * KICKBACK
+		vel.y += angle_vector.y * KICKBACK
